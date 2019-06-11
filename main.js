9457b6c76818772100ec00498b2093bc4add0b69
RH Blanchfield <dev@Alamedas-MacBook-Air.local>
2019-06-11 19:00:21 -0400

    Nutritional Data sidebar data loaded

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-//https://developer.edamam.com/admin/applications/1409618447375?service_id=2555417725632
-
-// Application Keys - 65c4e536f249fd469543ef31130ef33d
-
-// Application ID
-// df52775d //This is the application ID, you should send with each API request.
-//https://developer.edamam.com/edamam-docs-recipe-api
-//*Exactly one of these ( q / r ) must be present
-
-//q	yes*	string	Query text. For example q=chicken. This or the r parameter are required
-
-//r	yes*	string	Returns information about a specific recipe based on its ID ie. r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9b5945e03f05acbf9d69625138385408 This or the q parameter are required
-
-//app_id	yes	string	Your 3scale application ID
-//app_key	yes	string	Your 3scale application key(please note app_id / app_key are an ordered pair)
-
-
-//Example GET Request: curl "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
-
 console.log("Your JS is working");
 
 //API INFO
@@ -48,32 +29,39 @@ btn.addEventListener('click', async () => {
   render(foodItems)
 });
 
-let value;
+// FOOOD LIST
 
 const render = (foodItems) => {
   foodItems.forEach((food) => {
     console.log(food.recipe)
     console.log(foodItems)
     const list = document.querySelector('.food-detail')
-    const element = document.createElement('div');
+    const element = document.createElement('ul');
     element.innerHTML =
       `<h2>${food.recipe.label}</h2>
       <p>Health Label:${food.recipe.healthLabels}</p>
       <p>Diet:${food.recipe.dietLabels}</p>
-      <p>Calories: ${food.recipe.calories} </p>
+      <p>Calories: ${Math.round(food.recipe.calories)} </p>
+      <button> Nutrition Info </button>
        `;
+
+    //OPEN SIDEBAR NAV
+
+    const getRecipeBtn = element.querySelector('button');
+    getRecipeBtn.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const nutritionElement = document.querySelector(".sidenav");
+      const nutContainer = document.querySelector(".clearArea");
+      nutContainer.innerHTML = "";
+      food.recipe.digest.forEach((nutVal) => {
+        nutContainer.innerHTML +=
+          `
+          <p>${nutVal.label}: ${Math.round(nutVal.total)}${nutVal.unit}</p>
+          `;
+      })
+      nutritionElement.style.width = "500px";
+      getRecipeBtn.style.display = 'none';
+    });
     list.appendChild(element)
-  })
-}
-
-// HTML PAGE TRANSITIONS EVENT LISTENERS
-
-// page1.addEventListener("click", () => {
-//   page1.style.display = "none"
-//   page2.style.display = "block"
-// })
-
-// page2.addEventListener("click", () => {
-//   page2.style.display = "none"
-//   page1.style.display = "block"
-// })
+  });
+};
